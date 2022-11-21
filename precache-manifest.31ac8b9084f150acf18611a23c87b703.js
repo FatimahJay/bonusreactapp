e7efbe30317648bab5951262e89d76282389da87
Fatima <104238604+FatimahJay@users.noreply.github.com>
2022-11-21 22:10:30 +0500

    Updates

---
+++ precache-manifest.31ac8b9084f150acf18611a23c87b703.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e64651dcd0811dcf5e4bf3932dbb03d0",
+    "url": "/bonusreactapp/index.html"
+  },
+  {
+    "revision": "ed6ddae897d204b123e9",
+    "url": "/bonusreactapp/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "739d4561892396261251",
+    "url": "/bonusreactapp/static/css/main.fce36487.chunk.css"
+  },
+  {
+    "revision": "ed6ddae897d204b123e9",
+    "url": "/bonusreactapp/static/js/2.3515188a.chunk.js"
+  },
+  {
+    "revision": "739d4561892396261251",
+    "url": "/bonusreactapp/static/js/main.8630f7e2.chunk.js"
+  },
+  {
+    "revision": "bd038f6b1ebec31b6d2c",
+    "url": "/bonusreactapp/static/js/runtime~main.37130583.js"
+  }
+]);
